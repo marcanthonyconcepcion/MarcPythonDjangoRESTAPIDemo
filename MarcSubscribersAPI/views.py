5bf5b79aedd76cd04735b9a8b02f3dac980ca561
Marc Concepcion <marcanthonyconcepcion@gmail.com>
2020-08-05 23:36:22 +0800

    Use ViewSet to elegantly implement RESTful API POST, GET, PATCH, PUT and DELETE.

--- MarcSubscribersAPI/views.py
+++ MarcSubscribersAPI/views.py
@@ -7,22 +7,15 @@ from .serializers import SubscriberSerializer
 from .models import Subscriber
 
 
-@api_view(['GET', 'POST', 'PATCH', 'DELETE', 'PUT'])
-def subscribers(request):
-    # GET is RETURN ALL LIST
-    if request.method == 'GET':
-        if not request.data:
-            serializer = SubscriberSerializer(Subscriber.objects.all(), many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            email = request.data.get('email_address')
-            if not Subscriber.objects.filter(email_address=email).exists():
-                return Response("E-mail Address {email} not found.".format(email=email), status=status.HTTP_404_NOT_FOUND)
-            entry = Subscriber.objects.get(email_address=email)
-            serializer = SubscriberSerializer(entry)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-    # POST is REGISTRATION
-    elif request.method == 'POST':
+class SubscriberViewSet(viewsets.ModelViewSet):
+    queryset = Subscriber.objects.all().order_by('email_address')
+    serializer_class = SubscriberSerializer
+
+    def list(self, request):
+        serializer = SubscriberSerializer(Subscriber.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
         email = request.data.get('email_address')
         if Subscriber.objects.filter(email_address=email).exists():
             return Response("Subscriber with e-mail {email} already exists.".format(email=email), status=status.HTTP_409_CONFLICT)
@@ -31,26 +24,26 @@ def subscribers(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    # PATCH is Change Password
-    elif request.method == 'PATCH':
+
+    def retrieve(self, request, pk=None):
+        entry = Subscriber.objects.get(pk=pk)
+        serializer = SubscriberSerializer(entry)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        return Response("Illegal Operation. Totally replacing an existing subscriber is not allowed.", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, pk=None):
         if not request.data:
             return Response("Illegal Operation.", status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        if 'email_address' not in request.data:
-            return Response("Illegal Operation. No e-mail address specified.", status=status.HTTP_400_BAD_REQUEST)
         if 'password' not in request.data:
             return Response("Illegal Operation. No new password to change.", status=status.HTTP_400_BAD_REQUEST)
-        email = request.data.get('email_address')
-        if not Subscriber.objects.filter(email_address=email).exists():
-            return Response("E-mail Address not found.", status=status.HTTP_404_NOT_FOUND)
-        entry = Subscriber.objects.get(email_address=email)
+        entry = Subscriber.objects.get(pk=pk)
         password = request.data.get('password')
         entry.password = password
         entry.save()
         serializer = SubscriberSerializer(entry)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    # DELETE is not allowed.
-    elif request.method == 'DELETE':
+
+    def destroy(self, request, pk=None):
         return Response("Illegal Operation. Deleting a subscriber is not allowed.", status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    # PUT is not allowed.
-    elif request.method == 'PUT':
-        return Response("Illegal Operation. Totally replacing an existing subscriber is not allowed.", status=status.HTTP_405_METHOD_NOT_ALLOWED)
