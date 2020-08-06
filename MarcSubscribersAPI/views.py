6da0e76fd18cc96b331a59d980c419ac7a1f1ae3
Marc Concepcion <marcanthonyconcepcion@gmail.com>
2020-08-06 12:17:52 +0800

    Add send_mail method and extract to a method the generation of the serialized output message.

--- MarcSubscribersAPI/views.py
+++ MarcSubscribersAPI/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .serializers import SubscriberSerializer
 from .models import Subscriber
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class SubscriberViewSet(viewsets.ModelViewSet):
@@ -11,14 +13,14 @@ class SubscriberViewSet(viewsets.ModelViewSet):
 
     # GET to list all subscribers.
     def list(self, request):
-        output_message = {"user": "null", "errors": []}
+        output_message = self.generate_initial_output_message()
         serializer = SubscriberSerializer(Subscriber.objects.all(), many=True)
         output_message["user"] = serializer.data
         return Response(output_message, status=status.HTTP_200_OK)
 
     # POST to register new subscriber.
     def create(self, request):
-        output_message = {"user": "null", "errors": []}
+        output_message = self.generate_initial_output_message()
         email = request.data.get('email_address')
         if Subscriber.objects.filter(email_address=email).exists():
             output_message["errors"].append({
@@ -30,12 +32,14 @@ class SubscriberViewSet(viewsets.ModelViewSet):
             output_message["errors"].append(serializer.errors)
             return Response(output_message, status=status.HTTP_400_BAD_REQUEST)
         serializer.save()
+        token = "token"
+#       self.send_email(email, token)
         output_message["user"] = serializer.data
         return Response(output_message, status=status.HTTP_201_CREATED)
 
-    # GET with index to validate user.
+    # GET with index to for user activation.
     def retrieve(self, request, pk=None):
-        output_message = {"user": "null", "errors": []}
+        output_message = self.generate_initial_output_message()
         entry = Subscriber.objects.get(pk=pk)
         serializer = SubscriberSerializer(entry)
         output_message["user"] = serializer.data
@@ -43,13 +47,13 @@ class SubscriberViewSet(viewsets.ModelViewSet):
 
     # PUT should not be used.
     def update(self, request, pk=None):
-        output_message = {"user": "null", "errors": []}
+        output_message = self.generate_initial_output_message()
         output_message["errors"].append({"system": ["Illegal Operation."]})
         return Response(output_message, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     # PATCH to change password
     def partial_update(self, request, pk=None):
-        output_message = {"user": "null", "errors": []}
+        output_message = self.generate_initial_output_message()
         if 'password' not in request.data:
             output_message["errors"].append({"password": ["Field required."]})
             return Response(output_message, status=status.HTTP_400_BAD_REQUEST)
@@ -61,7 +65,19 @@ class SubscriberViewSet(viewsets.ModelViewSet):
         output_message["user"] = serializer.data
         return Response(output_message, status=status.HTTP_200_OK)
 
+    # DELETE should not be used.
     def destroy(self, request, pk=None):
-        output_message = {"user": "null", "errors": []}
+        output_message = self.generate_initial_output_message()
         output_message["errors"].append({"system": ["Illegal Operation."]})
         return Response(output_message, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def generate_initial_output_message(self):
+        return {"user": "null", "errors": []}
+
+    def send_email(self, email, token):
+        subject = 'send_email'
+        message = 'Here is your token authentication to validate your subscriber account.\n{token}.'.format(
+            token=token)
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email, ]
+        send_mail(subject, message, email_from, recipient_list)
